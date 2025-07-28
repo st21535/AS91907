949a3b5272dd06c113cddce451e3729bd7ec94d4
Zaira Lopez <st21535@howick.school.nz>
2025-07-29 09:45:37 +1200

    Reading off the json

--- ProgressTracker.py
+++ ProgressTracker.py
@@ -3,26 +3,40 @@ Progress Tracker
 
 '''
 
-
 from tkinter import * 
 import json
 class ProgressTracker:
     def __init__(self):
         self.root=Tk()
+        self.root.title("Progress Tracker!")
         self.container=Frame(self.root)
         self.container.grid(row=0,column=0,sticky="nesw")
         
         frame=Frame(self.container)
-        #Main Heading````
-        self.title_label=Label(frame,font="arial 16", text="Progress Tracker!")
-
-        self.title_label.grid(row=0,columnspan=3,padx=10,pady=10)
-
-        #getting name for project
+        frame.grid(row=0,column=0,padx=10,pady=10)
+        #Title
         self.prj_name_label = Label(self.root, font = "Arial 16", text = "Progress Tracker")
-        self.prj_name_label.grid(sticky = NSEW)
+        self.prj_name_label.grid(sticky = NSEW,row=1)
+        
+
+
+        with open("tasks.json", "r") as f:
+                self.tasks=json.load(f)
+        
+        self.tests=self.tasks[1]
+        print(self.tests["Project Name"])
+
+        for i, task in enumerate(self.tasks):
+            name_label = Label(frame, text=task["Project Name"], font="Arial 12", anchor="w", justify=LEFT)
+            name_label.grid(row=i+2, column=0, sticky=W, padx=10, pady=2)
+        
+        '''
+        name_label=Label(frame,text=self.tasks,font="arial 12", justify=LEFT)
+        name_label.grid(row=4,column=0,padx=10,pady=10)
+        '''
         
     def run(self):
         self.root.mainloop()
 app = ProgressTracker()
 app.run()
+
