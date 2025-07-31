267393a9096c786ffea0de867b9d205a4f6cd6c7
Zaira Lopez <st21535@howick.school.nz>
2025-08-01 10:30:26 +1200

    notworking

--- ProgressTracker.py
+++ ProgressTracker.py
@@ -11,24 +11,33 @@ class ProgressTracker:
         self.root.title("Progress Tracker!")
         self.container=Frame(self.root)
         self.container.grid(row=0,column=0,sticky="nesw")
+        self.root.geometry("600x600")
+
         
         frame=Frame(self.container)
         frame.grid(row=0,column=0,padx=10,pady=10)
-        #Title
-        self.prj_name_label = Label(self.root, font = "Arial 16", text = "Progress Tracker")
-        self.prj_name_label.grid(sticky = NSEW,row=1)
         
+        frame.rowconfigure(list(range(3),weight=1))
+        frame.columnconfigure(list(range(3),weight=1))
+
+        #Title
+        self.prj_name_label = Label(self.root, font = "Arial 16", text = "Title",justify=CENTER)
+        self.prj_name_label.grid(sticky="WE",row=0,columnspan=2)
 
 
         with open("tasks.json", "r") as f:
                 self.tasks=json.load(f)
         
-        self.tests=self.tasks[1]
-        print(self.tests["Project Name"])
 
-        for i, task in enumerate(self.tasks):
-            name_label = Label(frame, text=task["Project Name"], font="Arial 12", anchor="w", justify=LEFT)
-            name_label.grid(row=i+2, column=0, sticky=W, padx=10, pady=2)
+        self.sorted_order=sorted(self.tasks, key=lambda item: item["Level"])
+
+        for i, task in enumerate(self.sorted_order):
+            name_label = Label(self.root, text=task["Project Name"], font="Arial 12", )
+            name_label.grid(row=i+4, column=0, padx=10, pady=2)
+        
+        upcoming_label=Label(frame,text="Upcoming Due Dates",font="Arial 12")
+        upcoming_label.grid(row=7,column=3,pady=5,padx=5)       
+
         
         '''
         name_label=Label(frame,text=self.tasks,font="arial 12", justify=LEFT)
