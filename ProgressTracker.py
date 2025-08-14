d8a9de1893e7ba85ee38eaa70758be5685cd45f3
Zaira Lopez <st21535@howick.school.nz>
2025-08-14 12:37:55 +1200

    Add files via upload

--- ProgressTracker.py
+++ ProgressTracker.py
@@ -52,15 +52,13 @@ class ProgressTracker:
 
 
         self.sorted_order=sorted(self.tasks, key=lambda item: item["Level"])
-
         for i, task in enumerate(self.sorted_order):
             var=IntVar(value=1 if task["Progress"]==100 else 0)
-            self.checkbox_progress = Checkbutton(self.left_frame, text=f"{task['Project Name']}",command=self.update_progress,variable=var)
+
+            #Checkbox for progress
+            self.checkbox_progress = Checkbutton(self.left_frame, text=f"{task['Project Name']}",command=lambda v=var,t=task: self.completed(v,t),variable=var)
             self.checkbox_progress.grid(row=i, column=0, sticky="w")
-
-
-
-
+            #entry
             entry = Entry(self.left_frame, width=5)
             entry.insert(0, task["Progress"])
             entry.grid(row=i, column=1)
@@ -71,15 +69,33 @@ class ProgressTracker:
         for task in sorted_tasks:
             Label(self.right_frame, text=f"{task['Project Name']} : {task['Due Date']}").grid(sticky="nsew")
 
-    def update_progress(self,var):  
+    def completed(self,var,task):  
         if var.get()==1:
-            print("This has been checked off")
+            task["Progress"]=100
+            self.update_progress_bar() #change the progress bar
+            self.display_left_panel()
 
-        
+
+    def update_progress(self,task,entry):
+        num=int(entry.get())
+        if num >=100:
+           if num >= 100:
+               self.tasks = [t for t in self.tasks if t["Project Name"] != task["Project Name"]]
+        else:
+            for t in self.tasks:
+                if t["Project Name"]==task["Project Name"]:
+                    t["Progress"]=num
+                    break     
+        self.update_progress_bar()
+        self.display_left_panel()
+
 
     def display_progress_bar(self):
         pass
-       
+    def update_progress_bar(self):
+        pass
+
+
     def run(self):
         self.root.mainloop()
 app = ProgressTracker()
--- StudyClock.py
+++ StudyClock.py
@@ -1,21 +1,42 @@
 
 from tkinter import * 
+import time 
 import json
+from tkinter import ttk
 class StudyClock:
     def __init__(self):
         self.root=Tk()
         self.root.title("StudyClock!")
         self.container=Frame(self.root)
-        self.container.grid(row=0,column=0,sticky="nesw")
-        
-        frame=Frame(self.container)
-        frame.grid(row=0,column=0,padx=10,pady=10)
-        #Title
-        self.prj_name_label = Label(self.root, font = "Arial 16", text = "StudyClock")
-        self.prj_name_label.grid(sticky = NSEW,row=1)
+        self.root.geometry("600x400")
 
+        self.tasks=self.load_tasks()
+        self.int_mins=25 #How many mins in a cycle
+        self.tot_int=1
+        self.current_seconds=self.int_mins * 60
+
+        Label(self.root,text="Task Selector: ",font="arial 12").pack(pady=10,padx=10)
+        self.task_var=StringVar()
+
+
+        task_names=[task["Project Name"] for task in self.tasks] or ["here are no current tasks"]
+        self.task_menu=ttk.Combobox(self.root,textvariable=self.task_var,values=task_names,state="readonly")
+        self.task_menu.current(0)
+        self.task_menu.pack(pady=5)                                    
+
+    def load_tasks(self):
+        try:
+            with open("tasks.json", "r") as f:
+                content = f.read().strip()
+                if not content:
+                    return []
+                tasks = json.loads(content)
+        except (FileNotFoundError, json.JSONDecodeError):
+            tasks = []
+        return tasks
         
     def run(self):
         self.root.mainloop()
+
 app = StudyClock()
 app.run()
