425a049492a4120f41c5a19c1407898781031298
Zaira Lopez <st21535@howick.school.nz>
2025-08-22 12:17:17 +1200

    Add files via upload

--- FinalVers1.py
+++ FinalVers1.py
@@ -5,25 +5,31 @@ from tkinter import *
 
 class MainFile:
     def __init__(self):
-        if __name__=="__main__":
-            self.root = Tk()
-            self.root.title("Study Buddy")
-            self.root.geometry("600x400")
+        self.root = Tk()
+        self.root.title("Study Buddy")
+        self.root.geometry("600x400")
 
-        self.to_TM_button = Button(self.root, text="Task Manager", font="Arial 12 ", 
+        self.frame = Frame(self.root)
+        self.frame.pack(fill="both", expand=True)
+
+
+        self.button_frame = Frame(self.root)
+        self.button_frame.pack(side="bottom", fill="x", pady=10)
+
+
+        self.to_TM_button = Button(self.button_frame, text="Task Manager", font="Arial 12 ", 
                                   command=self.to_TMFrame)
-        self.to_TM_button.grid(row=1, column=0, padx=10, pady=10)
+        self.to_TM_button.pack(side="left", padx=5)
 
-        self.to_PT_button = Button(self.root, text="Progress Tracker", font="Arial 12 ", 
+        self.to_PT_button = Button(self.button_frame, text="Progress Tracker", font="Arial 12 ", 
                                   command=self.to_PTFrame)
-        self.to_PT_button.grid(row=1, column=2, padx=10, pady=10)
+        self.to_PT_button.pack(side="left", padx=5)
 
-        self.to_SL_button = Button(self.root, text="Study Tracker", font="Arial 12 ", 
+        self.to_SL_button = Button(self.button_frame, text="Study Tracker", font="Arial 12 ", 
                                   command=self.to_CLFrame)
-        self.to_SL_button.grid(row=1, column=3, padx=10, pady=10)
+        self.to_SL_button.pack(side="left", padx=5)
 
-        self.frame=Frame(self.root)
-        self.frame.grid(row=1,column=1,columnspan=2)
+
 
         self.root.mainloop()
 
@@ -44,9 +50,8 @@ class MainFile:
             widget.destroy()
 
 
+if __name__=="__main__":
+    MainFile()
 
-    def run(self):
-        self.root.mainloop()
-
-MainFile().run()
+    
     
